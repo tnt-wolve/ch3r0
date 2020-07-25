e0d0066bfb5b5a04d1bced627b2e8c09832223f7
Cyberbyte8869 <66154594+Cyberbyte8869@users.noreply.github.com>
2020-07-25 19:25:25 +0530

    Usage:-./install.sh after giving them the permission

---
+++ Install.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+clear
+
+BLACK='\e[30m'
+RED='\e[31m'
+GREEN='\e[92m'
+YELLOW='\e[33m'
+ORANGE='\e[93m'
+BLUE='\e[34m'
+PURPLE='\e[35m'
+CYAN='\e[36m'
+WHITE='\e[37m'
+NC='\e[0m'
+purpal='\033[35m'
+
+echo -e "${ORANGE} "
+echo ""
+echo "           .__    ________        _______         ";
+echo "      ____ |  |__ \_____  \______ \   _  \        ";
+echo "    _/ ___\|  |  \  _(__  <_  __ \/  /_\  \     ";
+echo "    \  \___|   Y  \/       \  | \/\  \_/   \    ";
+echo "     \___  >___|  /______  /__|    \_____  /   ";
+echo "         \/     \/       \/              \/   ";
+                     
+echo -e "${BLUE}   https://github.com/cyberbyte8869/ch3r0 ${NC}"
+
+echo -e "${RED}    [!] This Tool Must Run As ROOT [!]${NC}"
+echo ""
+echo -e ${CYAN}              "Select Best Option : "
+echo ""
+echo -e "${WHITE}              [1] Kali Linux / Parrot-Os "
+echo -e "${WHITE}              [0] Exit "
+echo -n -e "ch3r0 >> "
+read choice
+INSTALL_DIR="/usr/share/doc/ch3r0"
+BIN_DIR="/usr/bin/"
+if [ $choice == 1 ]; then 
+	echo "[*] Checking Internet Connection .."
+	wget -q --tries=10 --timeout=20 --spider http://google.com
+	if [[ $? -eq 0 ]]; then
+	    echo -e ${BLUE}"[✔] Loading ... "
+	    sudo apt-get update && apt-get upgrade 
+	    sudo apt-get install python-pip
+	    echo "[✔] Checking directories..."
+	    if [ -d "$INSTALL_DIR" ]; then
+	        echo "[!] A Directory ch3r0 Was Found.. Do You Want To Replace It ? [y/n]:" ;
+	        read input
+	        if [ "$input" = "y" ]; then
+	            rm -R "$INSTALL_DIR"
+	        else
+	            exit
+	        fi
+	    fi
+    		echo "[✔] Installing ...";
+		echo "";
+		git clone https://github.com/cyberbyte8869/ch3r0.git "$INSTALL_DIR";
+		echo "#!/bin/bash
+		python3 $INSTALL_DIR/ch3r0.py" '${1+"$@"}' > ch3r0;
+		sudo chmod +x ch3r0;
+		sudo cp ch3r0 /usr/bin/;
+		rm ch3r0;
+		echo ""; 
+		echo "[✔] Trying to installing Requirements ..."
+		sudo pip3 install lolcat
+		sudo apt-get install -y figlet
+		sudo pip3 install boxes
+		sudo apt-get install boxes
+		sudo pip3 install flask
+		sudo pip3 install requests
+	else 
+		echo -e $RED "Please Check Your Internet Connection ..!!"
+	fi
+
+    if [ -d "$INSTALL_DIR" ]; then
+        echo "";
+        echo "[✔] Successfuly Installed !!! ";
+        echo "";
+        echo "";
+        echo -e $ORANGE "		[+]+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++[+]"
+        echo 		"		[+]						      		[+]"
+        echo -e $ORANGE  "		[+]     ✔✔✔ Now Just Type In Terminal (ch3r0) ✔✔✔ 	[+]"
+        echo 		"		[+]						      		[+]"
+        echo -e $ORANGE "		[+]+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++[+]"
+    else
+        echo "[✘] Installation Failed !!! [✘]";
+        exit
+    fi
+elif [ $choice -eq 0 ];
+then
+    echo -e $RED "[✘] THank Y0u !! [✘] "
+    exit
+else 
+    echo -e $RED "[!] Select Valid Option [!]"
+fi
